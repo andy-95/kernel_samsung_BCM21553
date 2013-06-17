fb2b0b786898cb2baf138356e09e996de20b2644
kissingmylove <kissingmylove96@yahoo.com>
2013-06-17 11:35:23 +0700

    first commit

---
+++ common/include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 99 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#99 PREEMPT Sun Jun 16 12:03:12 ICT 2013"
+#define LINUX_COMPILE_TIME "12:03:12"
+#define LINUX_COMPILE_BY "halo"
+#define LINUX_COMPILE_HOST "kiss"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-68) "
